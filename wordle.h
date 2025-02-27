02ee764a4a39b2568497bb90bbd9b158f3b2f3b2
guyalster <guyalster@gmail.com>
2025-02-26 21:29:24 -0800

    Wordle Solver

---
+++ wordle.h
@@ -0,0 +1,35 @@
+#ifndef _WORDLE_H_
+#define _WORDLE_H_
+
+#include <cstddef>
+#include <string>
+#include <vector>
+#include <optional>
+
+class Wordle{
+    public:
+        Wordle(){}
+
+        Wordle(size_t wordSize, const std::string &pathToWords):wordSize(wordSize){
+            LoadDictionary(pathToWords);
+        }
+
+        Wordle(const Wordle& other) = delete;
+        Wordle(Wordle && other) = delete;
+        
+        ~Wordle(){}
+
+        std::optional<std::string> PlayRandomGame();
+        void PlayOffLineGame();
+        
+    private:
+
+        size_t wordSize;
+        std::vector<std::string> dictionary;
+        
+        void LoadDictionary(const std::string &path);
+        std::string GetNextBestGuess(const std::vector<std::string> &validWords);
+};
+
+
+#endif
